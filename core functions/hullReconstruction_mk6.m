8a26b168a1eb9b05a6bd2fe529ff5dc58c411cc7
AbbyLeung <abbyleung40@gmail.com>
2026-06-08 17:31:45 -0400

    removed plot generation (from debugging mode)

--- core functions/hullReconstruction_mk6.m
+++ core functions/hullReconstruction_mk6.m
@@ -620,7 +620,7 @@ parfor indd= 1: (endTrackingTime-startTrackingTime+1)  %parfor
         keyboard ; 
     end
     
-    if(1)
+    if(0)
         ww = [-1 1 -1 1] * 40  ; %#ok<UNRCH>
         figure(77); clf ;
         subplot(2,2,1) ;
